22ce9041064d8a49690231fe42d26030fdc7cc15
ja-rmo <itzheady@gmail.com>
2020-03-12 20:26:48 +0100

    under construstion php

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Jarmo.io</title>
+    <meta charset="utf-8">
+    <title>document</title>
+    <link rel="stylesheet" type="text/css" href="master.css">
 </head>
 <body>
-<?php echo "<p>this site is under constuction</p>" ?>
+    <header>
+        <h1>
+            <?php
+            $ip = $_SERVER['REMOTE_ADDR'];
+            echo "Hello, " . htmlspecialchars($ip);
+            ?>
+        </h1>
+    </header>
 </body>
 </html>
