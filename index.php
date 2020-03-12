64b79711d0b6d786a765384ab0efa90e138f950f
ja-rmo <itzheady@gmail.com>
2020-03-12 20:44:02 +0100

    under construstion php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>document</title>
-    <link rel="stylesheet" type="text/css" href="master.css">
-</head>
-<body>
-    <header>
-        <h1>
-            <?php
-            $ip = $_SERVER['REMOTE_ADDR'];
-            echo "Hello, " . htmlspecialchars($ip);
-            ?>
-        </h1>
-        <h1>this site is still under construstion, i am using it to practice php</h1>
-    </header>
-</body>
-</html>
+<?php
+
+
+$greeting = "Hello, World";
+
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Jarmo.io</title>
+    <link rel="stylesheet" type="text/css" href="master.css">
+</head>
+<body>
+<header>
+    <h1>
+        <?php echo $greeting ?>
+    </h1>
+    <h1>this website is still under construction, i am using it to practice php</h1>
+</header>
+</body>
+</html>
